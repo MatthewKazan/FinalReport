570d6ef82f991912b84b77ac64689686cbbf70af
Matt Kazan <Mattbkazan@icloud.com>
2022-04-14 17:04:20 -0400

    working naive autocorrect with multiprocessing and a bag of words language model, needs to be cleaned up

--- NaiveAutocorrect.py
+++ NaiveAutocorrect.py
@@ -1,10 +1,17 @@
+import concurrent.futures
+import multiprocessing
+from multiprocessing import Pool
 import json
+import threading
+from time import time
+
+import numpy as np
 
 alph = "abcdefghijklmnopqrstuvwxyz"
 
 misspelled_word = "ztay"
 
-weights = {"del_cost": 1, "ins_cost": 1, "rep_cost": 2}
+weights = {"del_cost": 2, "ins_cost": 1, "rep_cost": 2}
 
 dictionary = ["apple", "stay", "play"]
 
@@ -19,10 +26,14 @@ def process_original_json(filepath):
             proc_terms[term["term"]] += term["count"]
         else:
             proc_terms[term["term"]] = term["count"]
+    words = sorted(proc_terms.keys(), key=len)
+    final_terms = {}
+    for w in words:
+        final_terms[w] = proc_terms[w]
     with open("processed_words.json", "w") as outfile:
-        json.dump(proc_terms, outfile)
+        json.dump(final_terms, outfile)
 
-process_original_json('ap201001.json')
+#process_original_json('ap201001.json')
 
 with open('processed_words.json') as json_file:
     terms = json.load(json_file)
@@ -30,7 +41,6 @@ with open('processed_words.json') as json_file:
 total_words = 0
 for term in terms.keys():
     total_words += terms[term]
-
 
 def word_stats(term):
     return terms[term] / total_words
@@ -40,7 +50,6 @@ def minEditDistance(source, target):
     for i in range(len(source) + 1):
         temp = [0] * (1 + len(target))
         dists.append(temp)
-
     for i in range(1, len(source) + 1):
         dists[i][0] = dists[i - 1][0] + weights["del_cost"]
     for i in range(1, len(target) + 1):
@@ -55,7 +64,10 @@ def minEditDistance(source, target):
                                   dists[i - 1][j - 1] + weights["rep_cost"])
     return dists[len(source)][len(target)]
 
+oneEditDistances = {}
 def oneEditDistance(term):
+    if term in oneEditDistances.keys():
+        return oneEditDistances[term]
     splits = [(term[:i], term[i:]) for i in range(len(term) + 1)]
     deletes = [L + R[1:] for L, R in splits if R]
     replaces = [L + c + R[1:] for L, R in splits if R for c in alph]
@@ -64,43 +76,104 @@ def oneEditDistance(term):
     dict.extend(deletes)
     dict.extend(replaces)
     dict.extend(inserts)
-    return dict
+    oneEditDistances[term] = set(dict)
+    return oneEditDistances[term]
 
 def nEditDistance(n, term):
     dict = []
     prev_edits = oneEditDistance(term)
     for i in range(n - 1):
-        dict.extend(set(w2 for w1 in prev_edits for w2 in oneEditDistance(w1)))
+        prev_edits = [w2 for w1 in prev_edits for w2 in oneEditDistance(w1)]
+        dict.extend(prev_edits)
+        print(prev_edits)
     return set(w for w in dict if w in terms.keys())
 
+
+def findSimilarWords(n, word, low, ret_words):
+    for t in low:
+        if abs(len(t) - len(word)) >= n:
+            continue
+        dist = minEditDistance(word, t)
+        #print(t)
+        if dist < n:
+            ret_words.append((dist, t))
+            #print(ret_words[:])
+    return (ret_words)
+
+
 def naive_autocorrect(term):
-    potential = nEditDistance(3, term)
+    # potential = nEditDistance(2, term)
+    n = 8
+    potential = []#multiprocessing.Array('i', range(0))
+    num_threads = 12
+    words = np.array_split(list(terms.keys()), num_threads)
+    threads = []
+    pool = Pool(processes=num_threads)
+    for i in range(num_threads):
+        #print(words[i])
+        x = pool.apply_async(findSimilarWords, (n, term, words[i], []))
+        #x = threading.Thread(target=findSimilarWords, args=(n, term, words[i], potential))
+        # print(potential[:])
+        threads.append(x)
+    # pool.close()
+    # pool.join()
+    #     x.start()
+    for thread in threads:
+        # thread.join()
+        potential.extend(thread.get(timeout=1))
+    # print("in autocorrect", potential)
     maxWordProb = 0
-    bestWord = ""
-    for term in potential:
-        prob = word_stats(term)
-        if prob > maxWordProb:
+    bestWord = "???"
+    maxWordDist = 5
+
+    for (dist, t) in potential:
+        prob = word_stats(t)
+        if prob > maxWordProb and maxWordDist > dist:
             maxWordProb = prob
-            bestWord = term
+            maxWordDist = dist
+            bestWord = t
+            #print("first: ", bestWord)
+        elif maxWordDist > dist and prob < 50 / total_words:
+            maxWordProb = prob
+            maxWordDist = dist
+            bestWord = t
+            #print("2nd: ", bestWord)
+
+        elif prob / pow(dist, dist) > maxWordProb / pow(maxWordDist, maxWordDist):
+            maxWordProb = prob
+            maxWordDist = dist
+            bestWord = t
+            #print("3rd: ", bestWord)
+
+        #print(maxWordProb, maxWordDist, bestWord)
     return (maxWordProb, bestWord)
 
-def correct_sentance(input):
+def correct_sentance(input, autocorrectfn):
     corrected = []
     input = input.lower()
     for term in input.split(" "):
         if term in terms.keys():
             corrected.append((1, term))
         else:
-            corrected.append(naive_autocorrect(term))
+            corrected.append(autocorrectfn(term))
     return corrected
 # print(minEditDistance(misspelled_word, dictionary[0]))
-# process_original_json('ap201001.json')
+#process_original_json('ap201001.json')
 # print(word_stats("a"))
 # print(nEditDistance(4, "somthing"))
 
 
 #print(correct_sentance("This prigram automaticaly fixes spelling rrrora for th user somwht acuraty"))
+# print(minEditDistance("fantastically", "fatasically"))
+# print(total_words)
+# print(word_stats("terror") / 500)
+# print(word_stats("error") / 240)
 userInput = input("Enter a sentence with spelling errors to be corrected: ")
-sentence = " ".join([w for (p,w) in correct_sentance(userInput)])
-print("The program thinks you meant: ", sentence)
-print("With probabilities: ", correct_sentance(userInput))
+while not userInput == "":
+    cur_time = time()
+    output = correct_sentance(userInput, naive_autocorrect)
+    #print(time() - cur_time)
+    sentence = " ".join([w for (p,w) in output])
+    print("Autocorrect thinks you meant: ", sentence)
+    print("With probabilities: ", output)
+    userInput = input("Enter a sentence with spelling errors to be corrected: ")
